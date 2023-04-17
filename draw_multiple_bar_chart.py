b8b1acd1e1d5706ba7d623beeb2f32eafdbc5a22
Sampath Thennakoon <129614241+sampaththennakoon@users.noreply.github.com>
2023-04-17 16:53:12 +0530

    feat: update the file
    Add Train, Test & Validation

--- draw_multiple_bar_chart.py
+++ draw_multiple_bar_chart.py
@@ -7,17 +7,21 @@ import matplotlib.pyplot as plt
 def draw_chart():
     X = ['SA', 'LA', 'RA', 'SLA', 'SRA', 'DM', 'PC', 'JB', 'SL', 'BL', 'CL']
     Ytrain = [971, 108, 235, 165, 56, 688, 548, 120, 66, 127, 55]
-    Zvalidation = [117, 10, 25, 15, 2, 82, 63, 8, 6, 15, 6]
+    Yvalidation = [117, 10, 25, 15, 2, 82, 63, 8, 6, 15, 6]
+    Ztest = [353, 44, 92, 61, 22, 277, 228, 44, 28, 21, 49]
+
 
     X_axis = np.arange(len(X))
 
-    plt.bar(X_axis - 0.2, Ytrain, 0.4, label='Train')
-    plt.bar(X_axis + 0.2, Zvalidation, 0.4, label='Validation')
+    plt.figure(1, figsize=(15, 7.5))
+    plt.bar(X_axis - 0.2 , Ytrain, 0.2, label='Train')
+    plt.bar(X_axis, Yvalidation, 0.2, label='Validation')
+    plt.bar(X_axis + 0.2, Ztest, 0.2, label='Test')
 
     plt.xticks(X_axis, X)
     plt.xlabel("Road Marking Classes")
     plt.ylabel("Number of Occurrences")
-    plt.title("Road Marking Class Distribution Over The Train And Validation Data Set")
+    plt.title("Road Marking Class Distribution Over The Train, Validation And Test Data Set")
     plt.legend()
 
     plt.savefig('1.png', bbox_inches='tight')
